3d9729258c569eef87a7137c74e9420950e86d90
Joe Lothan <joe@lothan.net>
2021-12-18 11:35:32 -0600

    fixed permission issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,37 +4,35 @@ pacman -Syu --noconfirm
 pacman -S --noconfirm \
        $(tr "\n" " " < /vagrant/base)
 
-sudo systemctl enable gdm
-sudo systemctl start gdm
+systemctl enable gdm
+systemctl start gdm
 
 cd /opt
-sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R vagrant:vagrant yay
+git clone https://aur.archlinux.org/yay.git
+chown -R vagrant:vagrant yay
 cd yay
-makepkg -si
+su vagrant -c "makepkg -si --noconfirm"
 
-if [[ -f "/vagrant/hostname" ]]
-then
-    sudo cp /vagrant/hostname /etc/hostname
+if [[ -f "/vagrant/hostname" ]]; then
+   cp /vagrant/hostname /etc/hostname
 fi
 
-
-if [[ -f "/vagrant/.creds" ]]
-then
+if [[ -f "/vagrant/.creds" ]]; then
     USER=$(awk -F ':' '{print $1}' /vagrant/.creds)
     PASS=$(awk -F ':' '{print $2}' /vagrant/.creds)
-    useradd -m -s /bin/zsh $USER
+    useradd -m -s /usr/bin/zsh $USER
     echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USER
     echo "$USER:$PASS" | chpasswd
     # overwrite vagrant password
     (echo -n vagrant: && head -c 48 /dev/urandom | base64) | sudo chpasswd
+    chown -R $USER:$USER "/vagrant"
 else
     USER=vagrant
 fi
-
+    
 chmod +x "/vagrant/user-setup.sh"
 su $USER -c "/vagrant/user-setup.sh"
 
 # pretty gross
-yay -S --noconfirm \
-       $(tr "\n" " " < /vagrant/extra)
+su $USER -c 'yay -S --noconfirm \
+       $(tr "\n" " " < /vagrant/extra)'
